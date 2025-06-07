cd00d4535bf69c018d126505fe745678280a0eab
David Anson <david@dlaa.me>
2025-06-07 19:36:19 +0000

    Convert Power tab's two "Calibrate" dialogs.

--- src/js/utils/initializeModalDialog.js
+++ src/js/utils/initializeModalDialog.js
@@ -12,7 +12,7 @@ function getDialogTitleBar(messageId, messageParameters, onClose) {
     // HTML structure (TODO: Convert to flexbox)
     const dialogTitleBar = $(`
         <div class="jBox-title" style="height: 47px; padding: 15px 20px;">
-            <div style="float: left">${i18n.getMessage(messageId, messageParameters)}</div>
+            <div style="float: left">${i18n.getMessage(messageId, messageParameters || undefined)}</div>
             <div id="dialogclose" style="float: right; cursor: pointer; padding: 5px; margin: -5px;">
                 <svg width="10" height="10" version="1.1" xmlns="http://www.w3.org/2000/svg">
                     <line x1="0" y1="0" x2="10" y2="10" stroke="var(--surface-950)" stroke-width="2"/>
@@ -29,19 +29,25 @@ function getDialogTitleBar(messageId, messageParameters, onClose) {
 
 /**
  * Initializes a modal dialog from an HTML dialog element.
- * @param {JQuery.Selector} buttonSelector JQuery selector for the activation element.
+ * @param {JQuery.Selector|null} buttonSelector JQuery selector for the activation element.
  * @param {JQuery.Selector} dialogSelector JQuery selector for the dialog element.
  * @param {string} messageId Localized message identifier.
  * @param {object} [messageParameters] Localized message parameters
+ * @param {() => void} [onClose] Function invoked by close button.
  * @returns {HTMLDialogElement} HTML dialog element.
  */
-export function initializeModalDialog(buttonSelector, dialogSelector, messageId, messageParameters) {
+export function initializeModalDialog(buttonSelector, dialogSelector, messageId, messageParameters, onClose) {
     // Get dialog elements
     const dialog = $(dialogSelector);
     const dialogElement = dialog.get(0);
     const dialogContainerElement = dialog.children().first().get(0);
     // Add dialog title bar
-    dialog.prepend(getDialogTitleBar(messageId, messageParameters, () => dialogElement.close()));
+    dialog.prepend(
+        getDialogTitleBar(messageId, messageParameters, () => {
+            dialogElement.close();
+            onClose && onClose();
+        }),
+    );
     // Handle button click
     $(buttonSelector).on("click", () => {
         dialogElement.showModal();
